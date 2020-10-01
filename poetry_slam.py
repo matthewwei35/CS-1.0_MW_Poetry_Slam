fa93bdeca33f55871b3d1629a0539a99157747b2
Matthew Wei <matthewwei35@gmail.com>
2020-09-30 22:48:02 -0700

    Added lines_printed_smiley function

--- poetry_slam.py
+++ poetry_slam.py
@@ -1,5 +1,5 @@
 import random
-'''
+
 def get_file_lines(filename):
     infile = open(filename, "r")
     read_poem = infile.readlines()
@@ -7,9 +7,7 @@ def get_file_lines(filename):
     return read_poem
 
 print(get_file_lines("Caged_Bird.txt"))
-'''
 
-'''
 def lines_printed_backwards(lines_list):
     list_length = len(lines_list)
     lines_list = lines_list[::-1]
@@ -21,9 +19,7 @@ def lines_printed_backwards(lines_list):
 infile = open("Caged_Bird.txt", "r")
 print(lines_printed_backwards(infile.readlines()))
 infile.close()
-'''
 
-'''
 def lines_printed_random(lines_list):
     for random_line in range(len(lines_list)):
         random_line = random.choice(lines_list)
@@ -31,4 +27,13 @@ def lines_printed_random(lines_list):
 
 infile = open("Caged_Bird.txt", "r")
 print(lines_printed_random(infile.readlines()))
-'''
+infile.close()
+
+def lines_printed_smiley(lines_list):
+    for i in range(len(lines_list)):
+        smiley_line = ":)  " + lines_list[i] + "  (:"
+        print(smiley_line)
+
+infile = open("Caged_Bird.txt", "r")
+print(lines_printed_smiley(infile.readlines()))
+infile.close()
